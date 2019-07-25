4663279ad5fbbf876c27b81af3f422fda227b22f
Juanelo <juan@appsolution.co>
2019-07-25 09:06:28 -0700

    Initial Commit

---
+++ BZCamera/BZCamera.h
@@ -0,0 +1,19 @@
+//
+//  BZCamera.h
+//  BZCamera
+//
+//  Created by Juanelo on 7/25/19.
+//  Copyright © 2019 Juanelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BZCamera.
+FOUNDATION_EXPORT double BZCameraVersionNumber;
+
+//! Project version string for BZCamera.
+FOUNDATION_EXPORT const unsigned char BZCameraVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BZCamera/PublicHeader.h>
+
+
